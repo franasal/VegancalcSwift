15921bc285c7145111ca156e6b4a9ac631d2a434
El Pacho <farcilas@gmail.com>
2023-08-11 12:52:43 +0200

    Initial Commit

---
+++ veganCalc/veganCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  veganCalcApp.swift
+//  veganCalc
+//
+//  Created by Student on 11.08.23.
+//
+
+import SwiftUI
+
+@main
+struct veganCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
